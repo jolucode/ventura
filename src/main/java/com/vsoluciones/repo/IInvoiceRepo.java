e1ad59c97ee73a8a57efb5ca8f9f355e79d87073
Jose Becerra <becerra.pucp@gmail.com>
2024-09-19 19:00:09 -0500

    refactor: VSPERU-pagination refactor code

--- src/main/java/com/vsoluciones/repo/IInvoiceRepo.java
+++
@@ -1,6 +0,0 @@
-package com.vsoluciones.repo;
-
-import com.vsoluciones.model.Invoice;
-
-public interface IInvoiceRepo extends IGenericRepo<Invoice, String> {
-}
--- src/main/java/com/vsoluciones/repo/IMenuRepo.java
+++
@@ -1,6 +0,0 @@
-package com.vsoluciones.repo;
-
-import com.vsoluciones.model.Menu;
-
-public interface IMenuRepo extends IGenericRepo<Menu, String> {
-}
